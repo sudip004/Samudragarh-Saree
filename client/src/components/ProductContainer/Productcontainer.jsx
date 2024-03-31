41340f6e5e8e5a6d55550e24992b9dcf7766d3d3
Sudip Basak <84569249+sudip004@users.noreply.github.com>
2024-04-01 00:31:43 +0530

    Update Productcontainer.jsx

--- client/src/components/ProductContainer/Productcontainer.jsx
+++ client/src/components/ProductContainer/Productcontainer.jsx
@@ -17,7 +17,7 @@ const Productcontainer = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await axios.get("https://samudragarh-saree.onrender.com/api/saree");
+      const data = await axios.get("http://localhost:3000/api/saree");
       setSareeData(data.data);
     };
     fetchData();
